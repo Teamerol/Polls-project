d7db6655ec1c49c62ff38dae9b416e65cddd8680
Teamerol <fadeew.alex@mail.ru>
2021-10-17 04:12:10 +0300

    Implemented all types of accentuation.

--- services/accentuation_types.py
+++ services/accentuation_types.py
@@ -1,16 +1,23 @@
 from services.accentuation import Accentuation
 
 
-class Hyperthymicity(Accentuation):
+class Hyperthymic(Accentuation):
     """
     Active, energetic, extraversive people have this type of accentuation.
     """
     def __init__(self):
         super().__init__()
 
-class Cyclicity(Accentuation):
+class Cycloid(Accentuation):
     """
     Mood volatile, fickle people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Labile(Accentuation):
+    """
+    Prone to mood swings, not stable emotionally people.
     """
     def __init__(self):
         super().__init__()
@@ -22,21 +29,14 @@ class AsthenoNeuroticity(Accentuation):
     def __init__(self):
         super().__init__()
 
-class Sensitivity(Accentuation):
+class Sensitive(Accentuation):
     """
     Very sensitive, subtle, but not souciable people.
     """
     def __init__(self):
         super().__init__()
 
-class Psychasthenicity(Accentuation):
-    """
-    Indecisive, pensive, suspicious people.
-    """
-    def __init__(self):
-        super().__init__()
-
-class Psychasthenicity(Accentuation):
+class Psychasthenic(Accentuation):
     """
     Indecisive, pensive, suspicious people.
     """
@@ -46,6 +46,36 @@ class Psychasthenicity(Accentuation):
 class Schizoid(Accentuation):
     """
     Not prone to empaphy, eccentric, rather clever and inventive people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Epileptoid(Accentuation):
+    """
+    Usually affective, pedantic and adaptive people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Hysteroid(Accentuation):
+    """
+    Dramatic, creative, willing to be in spotlight people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Unstable(Accentuation):
+    """
+    People in this category don't have stable intersts, 
+    they can't concentrate on hard work, preferring entartainments.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Conformal(Accentuation):
+    """
+    These people try to be  indistinguishable from the environment.
+    They are 'products' of their surroundings.
     """
     def __init__(self):
         super().__init__()
