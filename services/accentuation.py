4c490f047b84035bbfec1ba26ce7684671aa505f
Teamerol <fadeew.alex@mail.ru>
2021-10-17 16:23:32 +0300

    added classes for all parameters and accentuations

--- services/accentuation.py
+++ services/accentuation.py
@@ -20,6 +20,7 @@ class Accentuation(ABC):
     @abstractmethod
     def __init__(self):
         self._score: int = 0
+        self.threshold_score: int = 5
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__} score = {self._score}"
@@ -28,8 +29,91 @@ class Accentuation(ABC):
         "Enchance score of accentuation."
         if isinstance(value, int):
             self._score += value
+        else:
+            raise TypeError(f"Score estimate {value} is not integer.")
     
     def decrease(self, value: int) -> None:
         "Reduce score of accentuation"
         if isinstance(value, int):
             self._score -= value
+        else:
+            raise TypeError(f"Score estimate {value} is not integer.")
+
+class Hyperthymic(Accentuation):
+    """
+    Active, energetic, extraversive people have this type of accentuation.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Cycloid(Accentuation):
+    """
+    Mood volatile, fickle people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Labile(Accentuation):
+    """
+    Prone to mood swings, not stable emotionally people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class AsthenoNeuroticity(Accentuation):
+    """
+    Hypochondriacs, extremely concerned about health people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Sensitive(Accentuation):
+    """
+    Very sensitive, subtle, but not souciable people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Psychasthenic(Accentuation):
+    """
+    Indecisive, pensive, suspicious people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Schizoid(Accentuation):
+    """
+    Not prone to empaphy, eccentric, rather clever and inventive people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Epileptoid(Accentuation):
+    """
+    Usually affective, pedantic and adaptive people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Hysteroid(Accentuation):
+    """
+    Dramatic, creative, willing to be in spotlight people.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Unstable(Accentuation):
+    """
+    People in this category don't have stable intersts, 
+    they can't concentrate on hard work, preferring entartainments.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Conformal(Accentuation):
+    """
+    These people try to be  indistinguishable from the environment.
+    They are 'products' of their surroundings.
+    """
+    def __init__(self):
+        super().__init__()
--- services/parameters.py
+++ services/parameters.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from enum import Enum
 
 
 class AdditionalParameter(ABC):
@@ -12,9 +14,12 @@ class AdditionalParameter(ABC):
         self._mark: int = 0
     
     def increase(self, value: int) -> None:
-        "Adds balls to the parameter mark."
+        "Adds points to the parameter mark."
         if isinstance(value, int):
             self._mark += value
+    
+    def get(self):
+        return self._mark
 
 class NegativeAttitude(AdditionalParameter):
     """
@@ -54,7 +59,67 @@ class Emansipation(AdditionalParameter):
 
 class Delinquent(AdditionalParameter):
     """
-    
+    Only for mans. Represents propensity, tendency to make antosocial, dangerous decisions.
     """
     def __init__(self):
         super().__init__()
+
+class MasculinityOrFeminity(AdditionalParameter):
+    """
+    Shows the predominance of having masculine or femine character traits.
+    """
+    is_masculine: bool = False
+    is_femine: bool = False
+
+    def __init__(self):
+        super().__init__()
+    
+    def compute(self, masculinity: Masculinity, feminity: Feminity):
+        """Count score for masculinity and feminity marks."""
+        self._mark = masculinity.get() - feminity.get()
+    
+    def result(self) -> NotImplemented:
+        """Returns result about predominance of masculine of femine traits."""
+        if self._mark > 0:
+            self.is_masculine = True
+        if self._mark < 0:
+            self.is_femine = True
+    
+class Masculinity(MasculinityOrFeminity):
+    """
+    Shows the predominance of having masculine character traits.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Feminity(MasculinityOrFeminity):
+    """
+    Shows the predominance of having femine character traits.
+    """
+    def __init__(self):
+        super().__init__()
+
+class Alcoholization():
+    """
+    Isn't inherited from class AdditionalParameter, 
+    because instance of Alcoholization wouldn't be shown on result graph.
+
+    Determines inclination to drinking alcohol.
+    """
+
+    def __init__(self) -> None:
+        self._score: int = 0
+
+    def increase(self, value: int) -> None:
+        "Enchance score of alcoholization."
+        if isinstance(value, int):
+            self._score += value
+        else:
+            raise TypeError(f"Score estimate {value} is not integer.")
+
+    def decrease(self, value: int) -> None:
+        """Reduce score of alcoholization"""
+        if isinstance(value, int):
+            self._score -= value
+        else:
+            raise TypeError(f"Score estimate {value} is not integer.")
